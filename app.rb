5501a6fcaedf983611ce847a264b4b83cf6d1493
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-05 15:41:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    get '/pirates.index' do
+      #Show all pirates
+      @pirates = Pirate.all
+      erb :'pirates/index'
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def initialize(params)
+    binding.pry
+
+    @@all << self
+  end
+
+  def self.all
+    @all
+  end
+
+  def self.clear
+    @all.clear
+  end
 end
